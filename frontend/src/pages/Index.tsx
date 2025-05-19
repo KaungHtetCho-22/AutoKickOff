be73902a02767f79dbf11de5511f0a5e212a3348
KaungHtetCho-22 <kaunghtetcho.ait@gmail.com>
2025-05-19 14:01:28 +0700

    first commit for frontend

---
+++ frontend/src/pages/Index.tsx
@@ -0,0 +1,12 @@
+
+import VideoGenerator from "@/components/VideoGenerator";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-blue-50 to-slate-100 py-12">
+      <VideoGenerator />
+    </div>
+  );
+};
+
+export default Index;
